67655001dd055260d7c59e7dc99ac5703b76cd0e
Kjell Ivar Storstein <kjelle@gmail.com>
2014-05-26 02:12:12 +0200

    Add components button
    Live edit

--- app.js
+++ app.js
@@ -1,34 +1,77 @@
 angular.module('dynamicAngular', ['ngSanitize', 'ngAnimate', 'mgcrea.ngStrap'])
-    .controller('MyController', ['$scope', function ($scope) {
-        $scope.options = [{
-            type: "jumbotron",
-            properties: [],
-            title: "Jumbotron of doom",
-            content: "Dette er <i>skikkelig</i> digg bra mann. <b>Dette er skikkelig bold mann</b>"
-        }, {
-            title: "Breadcrumbs er bra",
-            type: "breadcrumbs"
-        }, {
-            title: "Buttongroup er fint",
-            type: "buttongroup"
-        }, {
-            title: "Navbar er digg",
-            type: "navbar"
+    .controller('MyController', ['$scope', 'Components', function ($scope, Components) {
+
+        $scope.document = [{
+            type: "header",
+            title: "Dynamic directive prototype"
         }];
-        $scope.selectedDirective = $scope.options[0];
+
+        $scope.components = Components;
+
+        console.log(Components);
+
+        $scope.addComponent = function(key) {
+            var newComp = {};
+            angular.copy(Components[key], newComp);
+            newComp.type = key;
+            $scope.document.push(newComp);
+        };
 
     }])
     .directive('dynamicDirective', function() {
         return {
-            template: '<div bs-aside="dynamicDirective" ng-include="getTemplateUrl()"></div>',
+            template: '<div bs-aside="dynamicDirective" bs-aside-properties="properties" ng-include="getTemplateUrl()"></div>',
             scope: {
                 dynamicDirective: "="
             },
-            link: function(scope){
+            controller: function($scope, Components){
 
-                scope.getTemplateUrl = function() {
-                    return "partials/" + angular.lowercase(scope.dynamicDirective.type) + ".html";
+                var properties = [];
+                var type = $scope.dynamicDirective.type;
+                angular.forEach($scope.dynamicDirective, function(val, key){
+                    if(key === "type" || key === "$$hashKey"){
+                        return;
+                    } else {
+                        properties.push({
+                            "inputType": Components[type][key],
+                            "key": key
+                        });
+                    }
+                });
+                $scope.properties = properties;
+
+                $scope.getTemplateUrl = function() {
+                    return "partials/" + angular.lowercase($scope.dynamicDirective.type) + ".html";
                 };
+            }
+        };
+    })
+    .factory('Components', function () {
+        return {
+            jumbotron: {
+                title: "text",
+                content: "textarea",
+                btnText: "text",
+                button: "checkbox"
+            },
+            navbar: {
+                title: "text",
+                linkOne: "text",
+                linkTwo: "text",
+                linkThree: "text"
+            },
+            breadcrumbs: {
+                first: "text",
+                second: "text",
+                third: "text"
+            },
+            buttongroup: {
+                left: "text",
+                middle: "text",
+                right: "text"
+            },
+            header: {
+                title: "text"
             }
         };
     })
